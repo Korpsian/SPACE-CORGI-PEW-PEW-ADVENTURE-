42a008910b9409704e50779653ef8d803cbbe10c
Korbinian Maag <korbinian.maag@gmail.com>
2017-06-02 19:24:57 +0200

    Erstellen erster Grundbausteine des Gameplays

---
+++ Assets/scripts/BulletScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+    public float speed = 0; 
+
+    void Start()
+    {
+        Debug.Log("I am alive");
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime, Space.World);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log(coll);
+        Debug.Log("Hit");
+    }
+}
---
+++ Assets/scripts/PlayerScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public GameObject bullet;
+    public float horizontalSpeed = 0;
+    public float verticalSpeed = 0;
+    bool reloaded = true;
+
+    void FixedUpdate()
+    {
+        Movement();
+        Shoot();
+    }
+
+    void Movement()
+    {
+        if(Input.GetAxis("Vertical") < 0 )
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * verticalSpeed);
+            transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * -verticalSpeed);
+            transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+
+        }
+
+    }
+    void Shoot()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (boo)
+            {
+                Debug.Log("shooting");
+                StartCoroutine(Reloading(0.2f));
+                Vector2 thisOne = this.transform.position;
+                Instantiate(bullet, thisOne, Quaternion.identity);
+
+            }
+
+        }
+    }
+    //Reloading the Pew Pew
+    IEnumerator Reloading(float time)
+    {
+        reloaded = false;
+        yield return new WaitForSeconds(time);
+        reloaded = true;
+    }
+}
