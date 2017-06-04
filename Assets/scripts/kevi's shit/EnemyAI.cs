23bb301f75616cb24f749bb90eedcd37848b3315
Y4miyo <kevinhorcher@gmx.de>
2017-06-04 14:16:42 +0200

    AstarPathfinding
    Pathfinding hinzugefügt,
    script angefangen

---
+++ Assets/scripts/kevi's shit/EnemyAI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+[RequireComponent (typeof (Rigidbody2D))]
+[RequireComponent (typeof (Seeker))]
+public class EnemyAI : MonoBehaviour {
+
+    public Transform target;
+
+    //wie oft in einer sekunde geupdatet wird
+    public float updateRate = 2f;
+
+    private Seeker seeker;
+    private Rigidbody2D rb;
+
+    //berechneter Pfad
+    public Path path;
+
+    //KI Speed per second
+    public float speed = 300f;
+    public ForceMode2D fMode;
+
+    [HideInInspector]
+    public bool pathIsEnded = false;
+
+    //max distanz von KI zu anderem wegpunkt
+    public float nextWaypointDistance = 3;
+}
