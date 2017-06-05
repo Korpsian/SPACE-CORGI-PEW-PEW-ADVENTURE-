d73de89eea31ccf25f4179258df62183361d5f1a
Korbinian Maag <korbinian.maag@gmail.com>
2017-06-06 01:53:34 +0200

    Kleinere UI anpassungen und anfang Score Manager geschrieben

---
+++ Assets/scripts/korbi's shit/ScoreManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public GameObject Score;
+    int score = 0;
+    int points = 0;
+
+    private void Start()
+    {
+        StartCoroutine(UpdScoreOverTime(0f));
+    }
+
+    //Das sind die extra punkte die dazu kommen sollen, können auch negativ sein fürs abziehen
+    public void UpdScore(int newPoints)
+    {
+        points = points + newPoints;
+    }
+
+    private void Update()
+    {
+        if(points > 0)
+        {
+            points--;
+            score++;
+            StartCoroutine(UpdScoreOverTime(0.5f));
+        }
+
+        if(points < 0)
+        {
+            points++;
+            score--;
+            StartCoroutine(UpdScoreOverTime(0.5f));
+        }
+    }
+
+    IEnumerator UpdScoreOverTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Text temp = Score.GetComponent<Text>();
+        temp.text = "Score: " + score;
+        Debug.Log("I Updated Score");
+
+    }
+
+}
