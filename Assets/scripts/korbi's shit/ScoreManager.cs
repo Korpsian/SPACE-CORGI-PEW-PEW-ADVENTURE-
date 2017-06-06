a884f4fd74799add3853db4359d6d24f25c96b0d
Korbinian Maag <korbinian.maag@gmail.com>
2017-06-06 15:18:06 +0200

    UI Score manager v.0.5

--- Assets/scripts/korbi's shit/ScoreManager.cs
+++ Assets/scripts/korbi's shit/ScoreManager.cs
@@ -11,33 +11,75 @@ public class ScoreManager : MonoBehaviour {
 
     private void Start()
     {
-        StartCoroutine(UpdScoreOverTime(0f));
+        StartCoroutine(UpdScoreVisual(0f));
+        AddScore(1000);
     }
 
     //Das sind die extra punkte die dazu kommen sollen, können auch negativ sein fürs abziehen
-    public void UpdScore(int newPoints)
+    public void AddScore(int newPoints)
     {
         points = points + newPoints;
     }
 
     private void Update()
     {
-        if(points > 0)
+        UpdScorePositive();
+        UpdScoreNegative();
+
+    }
+
+    void UpdScorePositive()
+    {
+        if (points <= 9999999 && points >= 100000)
         {
-            points--;
-            score++;
-            StartCoroutine(UpdScoreOverTime(0.5f));
+            StartCoroutine(UpdScoreVisual(0.1f));
+            points = points - 10000;
+            score = score + 10000;
+        }
+        if (points <= 100000 && points >= 10000)
+        {
+            StartCoroutine(UpdScoreVisual(0.1f));
+            points = points - 1000;
+            score = score + 1000;
+        }
+        if (points <= 10000 && points >= 1000)
+        {
+            StartCoroutine(UpdScoreVisual(0.1f));
+            points = points - 100;
+            score = score + 100;
+        }
+        if (points <= 1000 && points >= 100)
+        {
+            StartCoroutine(UpdScoreVisual(0.1f));
+            points = points - 10;
+            score = score + 10;
+        }
+        if (points <= 100 && points > 0)
+        {
+            StartCoroutine(UpdScoreVisual(0.1f));
+            points = points - 1;
+            score = score + 1;
         }
 
-        if(points < 0)
+    }
+
+    void UpdScoreNegative()
+    {
+        if(points >= -1000 && points <= -100)
+        {
+            points = points + 10;
+            score = score - points;
+            StartCoroutine(UpdScoreVisual(0.1f));
+        }
+        if (points < 0 && points >= -100)
         {
             points++;
             score--;
-            StartCoroutine(UpdScoreOverTime(0.5f));
+            StartCoroutine(UpdScoreVisual(0.1f));
         }
     }
 
-    IEnumerator UpdScoreOverTime(float time)
+    IEnumerator UpdScoreVisual(float time)
     {
         yield return new WaitForSeconds(time);
         Text temp = Score.GetComponent<Text>();
