6b3378c68872b24d3013625a11b6fabdaf86e37d
Korbinian Maag <korbinian.maag@gmail.com>
2017-06-12 17:01:55 +0200

    Bossgegner Animation "BadCat" angefangen

---
+++ Assets/scripts/korbi's shit/EnemyMovement/EnemyRotationMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRotationMovement : MonoBehaviour {
+
+    public bool inverted = false;
+    public int speedMin = 5;
+    public int speedMax = 10;
+    int speed;
+
+	// Use this for initialization
+	void Start () {
+        speed = Random.Range(speedMin, speedMax);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (inverted)
+        {
+        transform.Rotate(Vector3.forward * Time.deltaTime * -speed);
+        }
+        else
+        {
+        transform.Rotate(Vector3.forward * Time.deltaTime * speed);
+        }
+    }
+}
