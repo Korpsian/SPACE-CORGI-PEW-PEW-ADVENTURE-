d71c39209c91ff756266db7b2c0203db69167eec
Y4miyo <kevinhorcher@gmx.de>
2017-06-15 01:21:08 +0200

    Enemy bullet sucked balls

---
+++ Assets/scripts/kevi's shit/EnemyBulletManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletManager : MonoBehaviour {
+
+    public GameObject EnemyBullet;
+    // Use this for initialization
+    public void InstantiateBullet()
+    {
+        Vector2 pos;
+        pos = this.transform.GetChild(0).transform.position;
+        Instantiate(EnemyBullet, pos, Quaternion.identity);
+    }
+}
---
+++ Assets/scripts/kevi's shit/EnemyShoot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour {
+
+    bool reloaded = true;
+    public float shootSpeed = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Shoot();
+	}
+
+    void Shoot()
+    {
+        if (reloaded)
+        {
+            StartCoroutine(Reloading(shootSpeed));
+
+            gameObject.GetComponent<EnemyBulletManager>().InstantiateBullet();
+        }
+
+    }
+    IEnumerator Reloading(float time)
+    {
+        reloaded = false;
+        //time = Random.Range(1f, 10f);
+        yield return new WaitForSeconds(time);
+        reloaded = true;
+    }
+}
